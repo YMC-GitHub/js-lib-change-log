4e89f7efcd431a3db27841da98872951548ed8ef
yemiancheng <ymc.github@gmail.com>
2020-02-03 07:49:34 +0800

    build: use itself

--- build/change-log.js
+++ build/change-log.js
@@ -1,31 +1,5 @@
-const fs = require('fs');
-const conventionalChangelog = require('conventional-changelog');
-let hasHim = fs.existsSync('CHANGELOG.md');
-// 缓存旧的
-let cache;
-if (hasHim) {
-  // console.log('read old');
-  cache = fs.readFileSync('CHANGELOG.md');
-}
-// console.log(cache.toString())
-// 获取新的
-const newData = conventionalChangelog({ preset: 'angular', releaseCount: 1 });
-const result = fs.createWriteStream('CHANGELOG.md', { encoding: 'utf-8', flags: 'w' });
-// 写入新的
-newData.pipe(result);
-// 写入旧的
-result.on('finish', () => {
-  // console.log('写入完成。');
-  if (cache) {
-    fs.createWriteStream('CHANGELOG.md', { encoding: 'utf-8', flags: 'a' }).write(cache);
-  }
-});
-
-/*
-const newData = conventionalChangelog({ preset: 'angular', releaseCount: 0 });
-const result = fs.createWriteStream('CHANGELOG.md', { encoding: 'utf-8', flags: 'w' });
-newData.pipe(result);
-*/
+const he = require('../lib/change-log.common.js');
+he.writeToFs();
 // file-usage
 // node ./build/change-log.js
 // note:i prefer to run in cm
